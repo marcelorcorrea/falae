6c4e7e252f1295230978659e6b33886b66a09b9b
Correa <marcelorcorrea@hp.com>
2017-05-09 17:34:08 -0300

    Add arrows to navigate among pages from a spreadsheet

--- app/src/main/java/com/marcelorcorrea/falae/fragment/PageFragment.java
+++ app/src/main/java/com/marcelorcorrea/falae/fragment/PageFragment.java
@@ -2,13 +2,17 @@ package com.marcelorcorrea.falae.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.marcelorcorrea.falae.R;
 import com.marcelorcorrea.falae.adapter.ItemPagerAdapter;
@@ -25,6 +29,8 @@ public class PageFragment extends Fragment implements ItemFragment.OnFragmentInt
     private OnFragmentInteractionListener mListener;
     private ViewPager mPager;
     private ItemPagerAdapter mPagerAdapter;
+    private ImageView leftNav;
+    private ImageView rightNav;
 
     public PageFragment() {
     }
@@ -51,10 +57,71 @@ public class PageFragment extends Fragment implements ItemFragment.OnFragmentInt
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_page, container, false);
+        leftNav = (ImageView) view.findViewById(R.id.left_nav);
+        rightNav = (ImageView) view.findViewById(R.id.right_nav);
         mPager = (ViewPager) view.findViewById(R.id.pager);
         mPagerAdapter = new ItemPagerAdapter(getChildFragmentManager(), page);
         mPager.setAdapter(mPagerAdapter);
+
+        if (shouldEnableNavButtons()) {
+            handleNavButtons();
+        }
+
+        leftNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enableNavButtons();
+//                mPager.arrowScroll(View.FOCUS_LEFT);
+                int tab = mPager.getCurrentItem();
+                if (tab > 0) {
+                    tab--;
+                    mPager.setCurrentItem(tab);
+                } else if (tab == 0) {
+                    mPager.setCurrentItem(tab);
+                }
+                handleNavButtons();
+            }
+        });
+        rightNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enableNavButtons();
+//                mPager.arrowScroll(View.FOCUS_RIGHT);
+                int tab = mPager.getCurrentItem();
+                tab++;
+                mPager.setCurrentItem(tab);
+                handleNavButtons();
+            }
+        });
+
         return view;
+    }
+
+    private void handleNavButtons() {
+        enableNavButtons();
+        if (shouldDisableLeftNavButton()) {
+            leftNav.setVisibility(View.INVISIBLE);
+        }
+        if (shouldDisableRightButton()) {
+            rightNav.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void enableNavButtons() {
+        leftNav.setVisibility(View.VISIBLE);
+        rightNav.setVisibility(View.VISIBLE);
+    }
+
+    private boolean shouldEnableNavButtons() {
+        return mPagerAdapter.getCount() > 1;
+    }
+
+    private boolean shouldDisableLeftNavButton() {
+        return mPager.getCurrentItem() == 0;
+    }
+
+    private boolean shouldDisableRightButton() {
+        return mPager.getCurrentItem() >= mPagerAdapter.getCount() - 1;
     }
 
     @Override
