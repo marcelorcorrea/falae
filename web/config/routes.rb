17bdbf37be8e380dcb0a928da0aaeb80beee5bb4
Leandro Manica and Matheus Longaray <pair+leandromanica+mlongaray@hp.com>
2017-05-15 14:31:51 -0300

    Create Category model.

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :roles
+
   resources :users do
     resources :spreadsheets do
       resources :pages
     end
   end
+
+  resources :categories
 end
