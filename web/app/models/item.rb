5342d349685d1321b9a8ac203ebb034eae18b566
Leandro Manica and Matheus Longaray <pair+leandromanica+mlongaray@hp.com>
2017-05-15 14:57:42 -0300

    Create CategoryItem association

--- web/app/models/item.rb
+++ web/app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :page
+  has_one :category_item
+  has_one :category, through: :category_item
 
   validates :name, :speech, :img_src, presence: true
 end
