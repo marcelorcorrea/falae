ed90299c47a2e47288b0ca463f637120fbb0dcfc
Matheus Longaray <longaray.matheus@gmail.com>
2017-06-11 23:54:38 -0300

    Configure action_mailer for development

--- web/config/environments/development.rb
+++ web/config/environments/development.rb
@@ -26,10 +26,11 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  #  Set action_mailer configuration for development
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
