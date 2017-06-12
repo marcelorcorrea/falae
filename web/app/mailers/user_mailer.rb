92e205216784a18c4d985acd6bd537150a0350e2
Matheus Longaray <longaray.matheus@gmail.com>
2017-06-11 23:54:11 -0300

    Update mailer for account activation

--- web/app/mailers/user_mailer.rb
+++ web/app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.account_activation.subject
   #
-  def account_activation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def account_activation(user)
+  	@user = user
+    mail to: user.email
   end
 end
