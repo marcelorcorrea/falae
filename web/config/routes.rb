56a4e75c7d04ce778da1df60e5cdb67f3a442788
Matheus Longaray <longaray.matheus@gmail.com>
2017-06-20 19:45:31 -0300

    Add password reset resource and mailer

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
   end
 
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   #post 'users/:id/items/add_to' => 'items#add_to', as: :add_to_my_items
 
