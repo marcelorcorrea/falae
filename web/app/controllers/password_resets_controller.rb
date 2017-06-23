d76632510de9724261bf62577c8019443879453c
Matheus Longaray <longaray.matheus@gmail.com>
2017-06-23 19:16:00 -0300

    Add password reset update action and validations

--- web/app/controllers/password_resets_controller.rb
+++ web/app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -14,4 +18,42 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+
+  def update
+    if @user.update_attributes(user_params)
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = t('user_mailer.password_reset.valid_reset')
+      redirect_to user_spreadsheets_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms a valid user.
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authentic_token?(:reset, params[:id]))
+        flash[:alert] = t('user_mailer.password_reset.invalid')
+        redirect_to root_url
+      end
+    end
+
+    # Checks expiration of reset token.
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:alert] = t('user_mailer.password_reset.expired_token')
+        redirect_to new_password_reset_url
+      end
+    end
 end
