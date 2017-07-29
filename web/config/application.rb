fe1ea1887066ea392dd3ebba3565e74e3aec4468
Leandro H. Manica <leandrohmanica@gmail.com>
2017-07-29 20:46:37 -0300

    Add more i18n for pt-br

--- web/config/application.rb
+++ web/config/application.rb
@@ -31,7 +31,7 @@ module Web
 
     # Whitelist locales available for the application
     I18n.available_locales = [:en, 'pt-BR']
- 
+
     # Set default locale to something other than :en
     I18n.default_locale = 'pt-BR'
   end
