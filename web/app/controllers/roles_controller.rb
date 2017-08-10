9d4019de25c99de42d895d52910a55a6932c0fb9
Leandro H. Manica <leandrohmanica@gmail.com>
2017-08-09 23:57:22 -0300

    Fix css indentation. Fix missing colon for method name.

--- web/app/controllers/roles_controller.rb
+++ web/app/controllers/roles_controller.rb
@@ -1,5 +1,5 @@
 class RolesController < ApplicationController
-  before_action authenticate!
+  before_action :authenticate!
   before_action :set_role, only: [:show, :edit, :update, :destroy]
 
   # GET /roles
