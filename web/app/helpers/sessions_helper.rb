6332ec10119892e6cbfbd2b0f95e4255cb95f1a5
Leandro H. Manica <leandrohmanica@gmail.com>
2017-11-14 11:22:24 -0200

    Fix NoMethodError for authenticate_with_http_token

--- web/app/helpers/sessions_helper.rb
+++ web/app/helpers/sessions_helper.rb
@@ -1,4 +1,6 @@
 module SessionsHelper
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
   # Logs in the given user.
   def log_in(user)
     if request.format.json?
@@ -12,7 +14,6 @@ module SessionsHelper
 
   # Returns the current logged-in user (if any).
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
     @current_user ||= get_user
   end
 
