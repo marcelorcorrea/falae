63caafdd112f42587592482e3f3a1e2985eb935f
Matheus Longaray <longaray.matheus@gmail.com>
2017-12-17 21:52:19 -0200

    Update host for action_mailer in production

--- web/config/environments/production.rb
+++ web/config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  host = 'falae-app.com'
+  host = 'falaeapp.org'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
