66f8a0da800796cdddfc5773c0d44c4421aed0a6
Leandro H. Manica <leandrohmanica@gmail.com>
2018-01-21 19:57:14 -0200

    Change root path to login

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,13 @@
 module HomeHelper
+  def home
+    if current_user
+      if request.path.start_with? user_path(current_user)
+        request.path
+      else
+        user_spreadsheets_path current_user
+      end
+    else
+      home_path
+    end
+  end
 end
