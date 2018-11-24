a7dd1f500c59f8a3af0942c07a7b6155d390a5e3
Leandro H. Manica <leandrohmanica@gmail.com>
2018-11-24 17:31:45 -0200

    Fix user unactivated ttl. Add logger for rufus scheduler.

--- config/initializers/job_scheduler.rb
+++ config/initializers/job_scheduler.rb
@@ -3,10 +3,20 @@ return unless $PROGRAM_NAME.include?('puma') && Rails.env == 'production'
 
 require 'rufus-scheduler'
 
-scheduler = Rufus::Scheduler.singleton lockfile: 'rufus-scheduler.lock'
+scheduler = Rufus::Scheduler.singleton lockfile: "#{Rails.root}/rufus-scheduler.lock"
+
+rufus_logger = Logger.new "#{Rails.root}/log/rufus.log"
 
 if scheduler.up?
   scheduler.cron '0 2 * * *' do
-    User.cleanup_unactivated
+    rufus_logger.warn "Running Cleanup Users #{Time.now}"
+    unactive_users = User.cleanup_unactivated
+    if unactive_users.any?
+      unactive_users.each do |user|
+        rufus_logger.warn "  Deleted user: #{user.name}, #{user.email}, #{user.created_at}"
+      end
+    else
+      rufus_logger.warn '  No unactivated users.'
+    end
   end
 end
