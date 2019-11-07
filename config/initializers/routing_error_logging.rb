d790e7b6486190e7cdcce3bfdcab95fe74b01d08
Leandro H. Manica <leandrohmanica@gmail.com>
2019-11-07 17:06:02 -0300

    Show only rounting error message in log with warning level

---
+++ config/initializers/routing_error_logging.rb
@@ -0,0 +1,20 @@
+# Overwriting log_error method from ActionDispatch::DebugExceptions to change
+# the log level for ActionController::RoutingError from fatal to warning, also
+# logging only the error message (excluding backtrace)
+
+module ActionDispatch
+  class DebugExceptions
+    alias old_log_error log_error
+
+    def log_error(request, wrapper)
+      logger = logger(request)
+      return unless logger
+
+      if exception.is_a?(ActionController::RoutingError)
+        logger.warn(wrapper.exception.message)
+      else
+        old_log_error request, wrapper
+      end
+    end
+  end
+end
