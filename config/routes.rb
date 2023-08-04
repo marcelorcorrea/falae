e094eb4c04cac7e120c81a3e063c38de798ad3aa
Leandro H Manica <leandrohmanica@gmail.com>
2023-08-04 18:57:26 -0300

    Upgrade to rails 6.1.7 (#122)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'sessions#new'
   get '/home', to: 'header_pages#home'
   get '/about', to: 'header_pages#about'
